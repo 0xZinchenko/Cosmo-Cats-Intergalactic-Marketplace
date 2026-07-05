c40a661902c61dce48ad737d717417a64b250d3a
Yan Zinchenko <12407564+YanZinchenko@users.noreply.github.com>
2026-07-05 16:14:01 +0200

    chore: initialize Spring Boot project

---
+++ src/main/java/com/zim4ik/spacecatmarket/CosmoCatsMarketplaceApplication.java
@@ -0,0 +1,13 @@
+package com.zim4ik.spacecatmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CosmoCatsMarketplaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CosmoCatsMarketplaceApplication.class, args);
+    }
+
+}
